8097667d264376e2c828762a100fa44c8400850f
spekie <179425982+spekie@users.noreply.github.com>
2025-06-01 21:52:23 -0400

    Update blocks.def.h

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "date '+%H:%M'",					5,		0},
+	{"", "date '+%H:%M'",					1,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
